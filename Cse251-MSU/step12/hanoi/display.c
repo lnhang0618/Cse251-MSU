09ff41b9967729ecc81d07d55013e8f10a011480
lnhang0618 <654344283@qq.com>
2022-12-14 23:23:13 +0800

    Really useful contents

---
+++ Cse251-MSU/step12/hanoi/display.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#include "hanoi.h"
+
+/* Add any define here */
+
+
+
+
+/*Add any function prototypes here */
+
+/*
+ * Display a disk of width p
+ */
+void DisplayDisk(int p)
+{
+    int i;
+    int spaceAround = (MaxDiskSize - p) / 2;
+
+    for(i=0; i<spaceAround;  i++)
+        printf(" ");
+
+    if(p == 0)
+    {
+        printf("|");
+    }
+    else
+    {
+        for(i=0;  i<p;  i++)
+            printf("O");
+    }
+
+    for(i=0;  i<spaceAround;  i++)
+        printf(" ");
+}
